eb7780532fe1e6a8aa06397e4baaaeb0f7e3eb1f
nbroeder <nbroeder@uwo.ca>
2021-08-06 20:57:06 -0400

    switched SideBarItems to SideBarItem

---
+++ src/components/SideBarItem.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+export const SideBarItem = (text) => {
+    return (
+        <div className ='side-bar-items'>
+            <button>{text}</button>
+        </div>
+    )
+}
