cc71954bd1b697df8a219c65b7fc66244117751a
nbroeder <nbroeder@uwo.ca>
2021-09-18 21:13:22 -0400

    created a section for each of my website topics.

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -6,7 +6,10 @@ export const MainSection = () => {
     return (
             <div className='main-section'>
                 <Section text={'About'}></Section>
-                
+                <Section text={'Projects'}></Section>
+                <Section text={'Skills'}></Section>
+                <Section text={'Resume'}></Section>
+                <Section text={'Contact Me'}></Section>
             </div>
 
     )
