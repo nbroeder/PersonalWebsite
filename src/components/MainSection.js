27c5680261202a85b6c4602b8b23ee5d30d513b3
nbroeder <nbroeder@uwo.ca>
2021-09-18 20:36:52 -0400

    Created an object Section that will go into the object main section. Each instance of Section will be one of the website sections (about, projects, resume, skills, and contact).

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -1,12 +1,32 @@
 import React from 'react';
-
+import {Section} from './Section'
 
 
 export const MainSection = () => {
     return (
             <div className='main-section'>
-                <h1 align='center' color='blue'>About</h1>
-                <text>I am a third year university student studying software engineering at Western University. I am typing more text because I want ot see what happens. </text>
+                <Section></Section>
+                <p>I am a third year university student studying software engineering at Western University. 
+                    I enjoy the outdoors and like to garden and go hiking. I also like to spend time reading and playing all kinds of games
+                    whether cards, boardgames, or sports. I have played soccer for as long as I can remember. 
+                    I also like running and one one day like to run a marathon.
+
+
+
+                    I am a third year university student studying software engineering at Western University. 
+                    I enjoy the outdoors and like to garden and go hiking. I also like to spend time reading and playing all kinds of games
+                    whether cards, boardgames, or sports. I have played soccer for as long as I can remember. 
+                    I also like running and one one day like to run a marathon.
+                    I am a third year university student studying software engineering at Western University. 
+                    I enjoy the outdoors and like to garden and go hiking. I also like to spend time reading and playing all kinds of games
+                    whether cards, boardgames, or sports. I have played soccer for as long as I can remember. 
+                    I also like running and one one day like to run a marathon.
+                    useDebugValue(I am a third year university student studying software engineering at Western University. 
+                    I enjoy the outdoors and like to garden and go hiking. I also like to spend time reading and playing all kinds of games
+                    whether cards, boardgames, or sports. I have played soccer for as long as I can remember. 
+                    I also like running and one one day like to run a marathon.)
+                    
+                </p>
             </div>
 
     )
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -5,7 +5,7 @@ import profile from './profilePicture.jpg'
 export const SideBar = () => {
     return (
         <div className='sideBar'>
-            <text className='side-bar-header'>Nicholas Broeders</text>
+            <text className='side-bar-header' align='center'>Nicholas Broeders</text>
             <img className='side-bar-image' src={profile} alt='' />
             <SideBarItem text={"about"}></SideBarItem>
             <SideBarItem text={"projects"}></SideBarItem>
