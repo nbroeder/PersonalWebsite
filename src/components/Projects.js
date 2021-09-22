f1ca4b97fc15c391678e57dd4ad3606d7192a20f
nbroeder <nbroeder@uwo.ca>
2021-09-22 13:14:44 -0400

    Added four properties to the Project object title, image,description, and link

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,19 @@
 import React from 'react'
 
-export const Projects = () => {
+
+    
+  
+
+export const Projects = props => {
     return (
         <div className='projects-body'>
+            <a href={props.link}>
+                <text>{props.title}</text>
+
+                <img src={props.image} alt="" />
+                <text>{props.description}</text>
+            </a>
+
             
         </div>
     )
--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import {Projects} from './Projects'
+import sample from './profilePicture.jpg'
+
+
+
 
 export const ProjectsSection = () => {
     return (
         <div className="section">
             <text className="section-header">Projects</text>
-            <Projects></Projects>
+            <Projects description={"this is a sample description"} title={"sampleTitle"} image={sample} link={"https://github.com/nbroeder"} ></Projects>
         </div>
     )
 }
