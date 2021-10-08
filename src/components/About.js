66493a4ca91e0075fce2f460512feea4f942965e
nbroeder <nbroeder@uwo.ca>
2021-10-08 18:43:55 -0400

    Added text to the about me section.

--- src/components/About.js
+++ src/components/About.js
@@ -3,27 +3,31 @@ import React from 'react'
 export const About = () => {
     return (
         <div className='about-body'>
-             <p>I am a third year university student studying software engineering at Western University. 
-                    I enjoy the outdoors and like to garden and go hiking. I also like to spend time reading and playing all kinds of games
-                    whether cards, boardgames, or sports. I have played soccer for as long as I can remember. 
-                    I also like running and one one day like to run a marathon.
+             <p>
+            Hello, and welcome to my website. My name is Nicholas Broeders and I am a third year software 
+             engineering student studying at Western University. I created this website so that I would have 
+             a place to easily share my projects and give people a chance to get to know a bit about me.
 
-
-
-                    I am a third year university student studying software engineering at Western University. 
-                    I enjoy the outdoors and like to garden and go hiking. I also like to spend time reading and playing all kinds of games
-                    whether cards, boardgames, or sports. I have played soccer for as long as I can remember. 
-                    I also like running and one one day like to run a marathon.
-                    I am a third year university student studying software engineering at Western University. 
-                    I enjoy the outdoors and like to garden and go hiking. I also like to spend time reading and playing all kinds of games
-                    whether cards, boardgames, or sports. I have played soccer for as long as I can remember. 
-                    I also like running and one one day like to run a marathon.
-                    useDebugValue(I am a third year university student studying software engineering at Western University. 
-                    I enjoy the outdoors and like to garden and go hiking. I also like to spend time reading and playing all kinds of games
-                    whether cards, boardgames, or sports. I have played soccer for as long as I can remember. 
-                    I also like running and one one day like to run a marathon.)
-                    
-                </p>
+            </p>
+            <p>
+            My passion for programming began back in high school when I was 15 and I took the introductory 
+            computer science course that my high school offered. I loved it. I loved that when I was 
+            programming, I could make anything that I could dream up and I spent the course making games and 
+            funny programs with my friends. 
+            </p>
+            <p>
+            I continued to take computer science in my two remaining years at high school and this is where I 
+            grew to enjoy the challenge that programming provided. I liked that creating programs made me think 
+            logically and I enjoyed finding the best solution to a problem that I was facing.
+            </p>
+            <p>
+            Studying at a western university the past three years has given me the opportunity to improve and 
+            grow both as a person and a programmer. I have been able to learn what is industry standard and how 
+            to not only create successful projects but how to write good code and how to work in teams in a 
+            professional environment. The last three years have only reinforced my passion for coding and no 
+            matter how many projects I work on I still get excited with the endless possibilities that each new 
+            project provides.
+            </p>
         </div>
     )
 }
