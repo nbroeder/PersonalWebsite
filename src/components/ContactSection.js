96aa95d640eae88f86e75b233aef7c3a1c72f6df
nbroeder <nbroeder@uwo.ca>
2021-10-08 19:21:39 -0400

    Added the correct images to my skills section

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -15,7 +15,7 @@ export const ContactSection = () => {
                 <Contact icon={phoneIcon} text='519-701-2110'></Contact>
                 <Contact icon={emailIcon} text='nbroeder@uwo.ca'></Contact>
                 <Contact icon={githubIcon} link={"https://github.com/nbroeder"} text={'nbroeder'}></Contact>
-                <Contact icon={linkedinIcon} link={"https://github.com/nbroeder"} text={'nbroeder'}></Contact>
+                <Contact icon={linkedinIcon} link={"https://www.linkedin.com/in/nicholas-broeders-6552251b7/"} text={'nbroeder'}></Contact>
             </div>
         </div>
     )
--- src/components/SkillsSection.js
+++ src/components/SkillsSection.js
@@ -1,16 +1,28 @@
 import React from 'react'
 import {Skills} from './Skills.js'
-import sampleImage from './images/profilePicture.jpeg'
+
+//Image files
+import javaIcon from './images/java.png'
+import javascriptIcon from './images/javascript.png'
+import pythonIcon from './images/python.png'
+import reactIcon from './images/react.png'
+import sqlIcon from './images/sql.png'
+
+
+
+
+
+
 export const SkillsSection = () => {
     return (
         <div className='section' >
             <text className='section-header'>Skills</text>
             <div className='row-contents'>
-                <Skills title={"Java"} image={sampleImage}></Skills>
-                <Skills title={"JavaScript"} image={sampleImage}></Skills>
-                <Skills title={"React"} image={sampleImage}></Skills>
-                <Skills title={"Python"} image={sampleImage}></Skills>
-                <Skills title={"SQL"} image={sampleImage}></Skills>
+                <Skills title={"Java"} image={javaIcon}></Skills>
+                <Skills title={"JavaScript"} image={javascriptIcon}></Skills>
+                <Skills title={"React"} image={reactIcon}></Skills>
+                <Skills title={"Python"} image={pythonIcon}></Skills>
+                <Skills title={"SQL"} image={sqlIcon}></Skills>
             </div>
         </div>
     )
