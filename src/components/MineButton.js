2f4240689bfe3dacb3d07531ff4371c08737853d
nbroeder <nbroeder@uwo.ca>
2023-09-11 10:31:08 -0700

    created function to calculate the number of mines touching a tile.

--- src/components/MineButton.js
+++ src/components/MineButton.js
@@ -2,15 +2,16 @@ import React from 'react';
 import Button from '@mui/material/Button';
 
 var mine = false;
-var clicked ;
+var clicked = false;
 var isClickedFunc;
 var index;
+var adjacentMines;
 
 
 export const MineButton = (props) => {
 
 
   return (
-    <Button variant="contained"  style={{maxWidth: '30px', maxHeight: '30px', minWidth: '30px', minHeight: '30px'}} onClick={() => props.isClickedFunc(props.index)}>{props.clicked === true?(props.mine?"Mine":"-"):""}</Button>
+    <Button variant="contained"  style={{maxWidth: '30px', maxHeight: '30px', minWidth: '30px', minHeight: '30px'}} onClick={() => props.isClickedFunc(props.index)}>{props.clicked === true?(props.mine?"Mine" : props.adjacentMines):""}</Button>
   )
 }
--- src/components/Minesweeper.js
+++ src/components/Minesweeper.js
@@ -4,7 +4,9 @@ import Button from '@mui/material/Button';
 import {MineButton} from './MineButton';
 
 var numMines = 10;
-var numTiles = 100;
+var boardHeight = 10;
+var boardWidth = 10;
+var numTiles = boardHeight * boardWidth;
 
 
 export const Minesweeper = () => {
@@ -19,6 +21,66 @@ export const Minesweeper = () => {
       if(arr.indexOf(r) === -1) arr.push(r);
     }
     return arr
+  }
+
+  function getAdjacentMines(mineArray, index){
+    var adjacentMines = 0;
+    var onLeftSide = false;
+    var onRightSide = false;
+    var onTopRow = false;
+    var onBottomRow = false;
+
+    if((index + 1) % boardWidth == 0){
+      onRightSide = true;
+    }else if(index % boardWidth == 0) {
+      onLeftSide = true;
+    }
+    if(index < boardWidth){
+      onTopRow = true;
+    }else if(index >= (boardWidth * boardHeight) - boardWidth){
+      onBottomRow = true;
+    }
+
+    if(onTopRow == false){
+      if(mineArray.includes(index - boardWidth)) 
+          adjacentMines++;
+
+      if(onLeftSide == false){
+        if(mineArray.includes(index - boardWidth - 1)) 
+          adjacentMines++;
+      }
+
+      if(onRightSide == false){
+        if(mineArray.includes(index - boardWidth + 1)) 
+          adjacentMines++;
+      }
+    }
+
+    if(onBottomRow == false){
+      if(mineArray.includes(index + boardWidth)) 
+          adjacentMines++;
+
+      if(onLeftSide == false){
+        if(mineArray.includes(index + boardWidth - 1)) 
+          adjacentMines++;
+      }
+
+      if(onRightSide == false){
+        if(mineArray.includes(index + boardWidth + 1)) 
+          adjacentMines++;
+      }
+    }
+      
+    if(onLeftSide == false){
+      if(mineArray.includes(index - 1)) 
+          adjacentMines++;
+    }
+    if(onRightSide == false){
+      if(mineArray.includes(index + 1)) 
+          adjacentMines++;
+    }
+
+    return adjacentMines;
   }
 
   function setBtnsToUnclicked(numTiles){
@@ -48,7 +110,7 @@ export const Minesweeper = () => {
       <Grid container spacing = {0} columns={10} width={350} height = {350}>
         {Array.from(Array(100)).map((_, index) => (
         <Grid xs={1} key={index}>
-          <MineButton  isClickedFunc = {testClick} index = {index} mine = {mineLocations.includes(index) === true? true : false} clicked = {btnClicked[index]  === 0 ? false : true} ></MineButton>
+          <MineButton  isClickedFunc = {testClick} index = {index} mine = {mineLocations.includes(index) === true? true : false} clicked = {btnClicked[index]  === 0 ? false : true} adjacentMines = {getAdjacentMines(mineLocations, index)}></MineButton>
         </Grid>
         ))}
       </Grid>
